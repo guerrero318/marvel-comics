6b6fe3f3d53805f8a520d8c6d3b9152c9ab22b3e
Bryan Guerrero <bryan@Bryans-MacBook-Pro.local>
2022-03-25 12:05:39 -0700

    changed routes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,15 +1,19 @@
 const axios = require("axios");
+const Comic = require("../models/Comic");
+const asyncWrap = require("../middleware/async");
 
-exports.getAllComics = (req, res) => {
+exports.getAllComics = asyncWrap(async (req, res) => {
   // make a get request to /api/users
-  axios
-    .get("http://localhost:5003/api/v1/marvelcomics")
-    .then((response) => {
-      const data = response.data.data;
-      res.render("store", { pageTitle: "Comic Labs", data: data });
-    })
-    .catch((err) => console.log(err));
-};
+  // axios
+  //   .get("http://localhost:5003/api/v1/marvelcomics")
+  //   .then((response) => {
+  //     const data = response.data.data;
+  //     res.render("store", { pageTitle: "Comic Labs", data: data });
+  //   })
+  //   .catch((err) => console.log(err));
+  const comics = await Comic.find();
+  res.render("store", { pageTitle: "Comic Labs", data: comics });
+});
 
 exports.getAddComic = (req, res) => {
   res.render("addComic", {
@@ -17,15 +21,28 @@ exports.getAddComic = (req, res) => {
   });
 };
 
-exports.getUpdateComic = (req, res) => {
+exports.getUpdateComic = asyncWrap(async (req, res) => {
   const id = req.query.id;
-  axios
-    .get(`http://localhost:5003/api/v1/marvelcomics/${id}`)
-    .then((comicData) => {
-      res.render("updateComic", {
-        pageTitle: "Update Comic",
-        data: comicData.data.data,
-      });
-    })
-    .catch((err) => console.log(`ADIMIN CONTROLLER ${err}`));
-};
+  // axios
+  //   .get(`http://localhost:5003/api/v1/marvelcomics/${id}`)
+  //   .then((comicData) => {
+  //     res.render("updateComic", {
+  //       pageTitle: "Update Comic",
+  //       data: comicData.data.data,
+  //     });
+  //   })
+  //   .catch((err) => console.log(`ADIMIN CONTROLLER ${err}`));
+  let comic = await Comic.findOneAndUpdate(id);
+
+  if (!comic) {
+    return res.status(404).send(`Comic not found with id: ${req.params.id}`);
+  }
+  comic = await Comic.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.render("updateComic", {
+    pageTitle: "Update Comic",
+    data: comic,
+  });
+});
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const adminController = require("../controllers/adminController");
+
 const { ensureAuthenticated } = require("../config/authentication");
 // /add-comic => GET
 router.get("/add-comic", ensureAuthenticated, adminController.getAddComic);
